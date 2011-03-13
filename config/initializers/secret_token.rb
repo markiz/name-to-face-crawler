b58b3c62a7d509b26678a4e34c950337f4aa64d7
Mark Abramov <markizko@gmail.com>
2011-03-12 19:04:01 -0500

    Freshly generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NameToFaceCrawler::Application.config.secret_token = '9fc7d2f51068e0f11b3e8354e53881fa2e8d09f01c6e0a3e344ebeaac55370d2ab74be1ae417ab28e577cd6a48bc6ef831e806308acf259bfe75d231a4681eeb'
